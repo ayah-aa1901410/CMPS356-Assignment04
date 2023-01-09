5f2bbc95fcf26b40b0697fbc321b7552232e47ea
ayah-aa1901410 <72663982+ayah-aa1901410@users.noreply.github.com>
2023-01-09 17:41:40 +0300

    changes

--- pages/api/identifier.js
+++ pages/api/identifier.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
           }
         })
       console.log("returning the id" + id);
-      res.status(200).json(JSON.stringify(id))
+      res.status(200).json(id)
     }else if(req.method === "POST"){
       console.log("inside verifying");
       const file = await fs.promises.readFile("tmp/users.json");
