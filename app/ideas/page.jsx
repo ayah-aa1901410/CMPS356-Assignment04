c9a42643c3a6852923fcd93a7b76cfbd4989bbf0
ayah-aa1901410 <72663982+ayah-aa1901410@users.noreply.github.com>
2023-01-09 17:43:15 +0300

    more changes

--- app/ideas/page.jsx
+++ app/ideas/page.jsx
@@ -40,7 +40,7 @@ export default function IdeasPage() {
 
       }else{
         function fetchData(){
-          const id = axios.get("/api/identifier")
+          const id = axios.get("/api/identifier").then(res => res.json())
           localStorage.setItem("user", id)
           setUserID(id);
         }
@@ -49,7 +49,7 @@ export default function IdeasPage() {
       }
     }else{
       function fetchData(){
-        const id = axios.get("/api/identifier")
+        const id = axios.get("/api/identifier").then(res => res.json())
         localStorage.setItem("user", id)
         setUserID(id);
       }
